e18442300baffbd57e87aaa57fc6fff3dd04dcc5
AlynneRaquel <alynneraquel@hotmail.com>
2023-10-30 20:12:49 -0300

    First commit

---
+++ src/main/java/com/example/messageriarabbitmqdocker/MessageriaRabbitmqDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.messageriarabbitmqdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageriaRabbitmqDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageriaRabbitmqDockerApplication.class, args);
+	}
+
+}
